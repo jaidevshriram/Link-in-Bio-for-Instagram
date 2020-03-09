98373288234e1ae1de96a52f1729b919c79942bb
Jaidev Shriram <jaidevtrumpet@gmail.com>
2020-03-09 18:06:52 +0530

    working on login

---
+++ linkinbio/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.models import User 
+from .models import *
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request,user)
+                return redirect('/')
+            else:
+                return HttpResponse("Your account was inactive.")
+        else:
+            print(request.POST)
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            return HttpResponse("Invalid login details given")
+    else:
+        return render(request, 'dashboard/login.html')
