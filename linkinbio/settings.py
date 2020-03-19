61759e15a4fd40f8a2b3c88ce0abd46aa8796f53
Jaidev Shriram <jaidevtrumpet@gmail.com>
2020-03-19 15:45:16 +0530

    Minor Update, Standard Message

--- linkinbio/settings.py
+++ linkinbio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&$06!ikh$)cz1b(wf5itc7bzy0m&_1q87$j7u6^8tyfhxst&dj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -125,15 +125,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
